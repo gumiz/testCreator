4b5e2aac0154c28262ffc93c259df5d212b4b387
gumiz <micgrn@gmail.com>
2015-04-15 23:00:04 +0200

    x

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
 var gzippo = require('gzippo');
 var express = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
  
-app.use(express.logger('dev'));
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser()); // pull information from html in POST
+app.use(methodOverride());  // simulate DELETE and PUT
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
