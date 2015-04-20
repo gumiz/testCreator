46f1bd96a183d6e41fff305e44e6e627608c860f
gumiz <m@m.mm>
2015-04-20 16:07:45 +0200

    dialogService.js notifyService.js

---
+++ app/scripts/services/notifyService.js
@@ -0,0 +1,22 @@
+/*global appServices, $ */
+appServices.factory('notifyService', function () {
+	"use strict";
+
+	function showMessage(message) {
+		return notification.message(message);
+	}
+
+	function showErrorMessage(message) {
+		return notification.error(message);
+	}
+
+	function alert(message) {
+		return notification.message(message);
+	}
+
+	return {
+		showMessage: showMessage,
+		showErrorMessage: showErrorMessage,
+		alert: alert
+	};
+});
